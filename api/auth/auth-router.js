0ba9d09573b08be68030428d023a6efc504c160d
Bethany Ingraham <BethFIngraham@gmail.com>
2024-10-16 16:09:22 -0400

    Auth router wired

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,8 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+const express = require('express');
 
+const router = express.Router();
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -24,7 +26,9 @@
     "message": "Password must be longer than 3 chars"
   }
  */
-
+router.post('/register', (req, res, next) => {
+  res.json('registering user');
+});
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,7 +45,9 @@
     "message": "Invalid credentials"
   }
  */
-
+router.post('/login', (req, res, next) => {
+  res.json('logging in user');
+});
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +64,16 @@
     "message": "no session"
   }
  */
+router.get('/logout', (req, res, next) => {
+  res.json('logging out user');
+});
 
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message || 'Something went wrong with the server',
+    stack: err.stack
+  });
+});
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
